b9d72d28c900ea2e41b5a8dd408714b66dd3c68e
lee <sir.knightlee@googlemail.com>
2026-03-16 14:37:29 +0000

    Final changes made, can now create aother projects with this as a template

---
+++ source/dummy.cpp
@@ -0,0 +1,4 @@
+// This file exists only to allow the template project to compile.
+// You can safely delete it once you add real source files.
+
+#warning "This file is a placeholder and should be deleted once you add real source files."
